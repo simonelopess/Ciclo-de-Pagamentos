b07dcf42de3b845ecdb88a4933e19355a7c3267b
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-03-31 18:21:11 -0300

    Atualização do mongoose

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise; //correção de bugs
-module.exports = mongoose.connect('mongodb://localhost/mymoney');
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true });
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
 mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o valor mínimo de '{MIN}' "
