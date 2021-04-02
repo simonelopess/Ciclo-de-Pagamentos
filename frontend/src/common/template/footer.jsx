6e7cc307b7965da95984e46f44142eaa26d2b187
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-04-02 12:29:18 -0300

    Rodapé

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Copyright &copy; 2021
+        <a href="#" target="_blank"> Simone Lopes</a>.
+        </strong>
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,6 +2,7 @@ import '../common/template/dependencias'
 import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props =>
 
@@ -9,5 +10,9 @@ export default props =>
     <div className='wrapper'>
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Coteudo</h1>
+        </div>
+        <Footer />
     </div>
 )
