0e04b9e05427f25ef81186e0855c8d096eb6abfd
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-04-02 11:16:32 -0300

    Criação do sideBar

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,11 +1,13 @@
 import '../common/template/dependencias'
 import React from 'react'
 import Header from '../common/template/header'
+import SideBar from '../common/template/sideBar'
 
 export default props =>
 
 (
     <div className='wrapper'>
         <Header />
+        <SideBar />
     </div>
 )
